13ea0dc5a941ce71a4945c9f8b91fa6069d30b92
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2021-02-11 17:40:33 +0530

    style: style updated

--- homepage.js
+++ homepage.js
@@ -1,29 +1,25 @@
-var theApi = 'https://fakestoreapi.com/products';
-var products = [];
+var apiURL = 'https://fakestoreapi.com/products';
 
-fetch(theApi)
+fetch(apiURL)
 .then(function(data){
     return data.json();
 })
-.then(function(data){
-    products = data;
+.then(function(products){
     products.forEach(function(item){
     console.log(item)
     var container = document.querySelector(".container").appendChild(document.createElement('div'))
     container.className = "sub-container";
     container.id = `${item.id}`;
     container.innerHTML = `<div class="item-container" id="${item.id}">
-    <p class="item-title">${item.title}</p>
+    <button class="wish-btn">♥</button>
+    <div class="product-container">
     <img src="${item.image}" alt="${item.category}" class="image">
-    
+    <p class="item-title">${item.title}</p>
     <div class="buy-section">
-    <p class="item-price">${"₹" + Math.round(item.price*72.85)}</p>
-    <button class="wish-btn">♥</button>    
+    <p class="item-price">${"$" + item.price}</p>    
     </div>
-    <button class="buy-btn"><a href="../product-brief/product.html">Buy<img src="https://img.icons8.com/nolan/25/fast-cart.png"/></a></button>
-    <p class="item-description">DESCRIPTION: \n\n ${item.description}</p>
+    <button class="buy-btn"><a href="../product-brief/product.html">Buy<img class="cart-img" src="./img/carts.svg"/></a></button>
     </div>`
     })
 })
-
 
