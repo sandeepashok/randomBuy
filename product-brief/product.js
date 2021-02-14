1056f4f27956977139c93132b1df0b6b878d1138
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2021-02-14 17:25:59 +0530

    chore page added

--- product-brief/product.js
+++ product-brief/product.js
@@ -15,33 +15,12 @@ fetch(apiURL)
         container.innerHTML = `<p class="product-title">${item.title}</p>
         <img src="${item.image}" alt="product-image" class="product-buy-img">
         <p class="product-price">${"Price: $" + item.price}</p>
-        <div class="form">
-        <div class="name">
-            <div class="name-head head">First Name:</div>
-            <input type="text" class="firstname" placeholder="First Name">
-            <div class="name-head head">Last Name:</div>
-            <input type="text" class="lastname" placeholder="Last Name">
+        <p class="product-description"><span class="product-desc-head">Description:</span> +${item.description}</p>
+        <div class="product-btns">
+        <button class="btn">Add to cart</button>
+        <button class="btn">Buy now</button>
         </div>
-        <div class="mail">
-            <div class="mail-head head">E-Mail:</div>
-            <input type="email" class="mail-input" placeholder="xyz@something.com">
-        </div>
-        <div class="address">
-            <div class="address-head head">Address:</div>
-            <input type="text" id="address-input" class="address-input" placeholder="Address"></input>
-        </div>
-        <div class="state">
-            <div class="state-head head">State:</div>
-            <input type="text" class="state-input" placeholder="State/Province">
-        </div>
-        <div class="country">
-            <div class="country-head head">Country:</div>
-            <input type="text" class="country-input" placeholder="country">
-        </div>
-        <div class="postal">
-            <div class="postal-head head">Postal/Zip Code:</div>
-            <input type="number" min="1" class="postal-code" placeholder="Postal/Zip Code">
-        </div>
-        <button class="place-order">Place Order</button>
-    </div>`
+        `
+
+
     })
